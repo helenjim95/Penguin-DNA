0fa36c90cf30df3d7384916c6031283f3eae2ac0
Mariya Koeva <mariya.koeva@tum.de>
2022-10-14 09:54:52 +0200

    Exercise-Template pushed by Artemis

---
+++ src/de/tum/in/ase/PenguinDNA.java
@@ -0,0 +1,19 @@
+package de.tum.in.ase;
+
+public class PenguinDNA {
+    public static boolean checkChains(String chain1, String chain2) {
+        //TODO: Check if two chains can bind to eachother
+        return false;
+    }
+
+    public static String createMatchingChain(String chain1) {
+        //TODO: Create a matching chain for chain1
+        return null;
+    }
+
+    public static boolean isPenguinDNA(String chain1, String chain2, String sequence) {
+        //TODO: Checking penguin DNA
+        return false;
+    }
+
+}
